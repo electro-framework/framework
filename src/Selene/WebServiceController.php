4fc7b39b1938825a30764eaa7edd2c399997fcb7
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-13 17:47:26 +0100

    Added support for POST with json responses to the WebServiceController.

--- src/Selene/WebServiceController.php
+++ src/Selene/WebServiceController.php
@@ -1,16 +1,32 @@
 <?php
 namespace Selene;
 
-class WebServiceController extends Controller {
+use ReflectionException;
+use ReflectionObject;
+use Selene\Exceptions\BaseException;
+use Selene\Exceptions\Status;
 
+class WebServiceController extends Controller
+{
   public $isWebService = true;
+  public $renderOnPOST = true;
+  public $responseData = null;
 
-  //--------------------------------------------------------------------------
-  protected function initialize()
-  //--------------------------------------------------------------------------
+  function render ()
   {
-    parent::initialize();
-    $this->page->contentIsXML = true;
+    return $this->responseData;
   }
 
+  protected function doFormAction (DataObject $data = null)
+  {
+    $this->getActionAndParam ($action, $param);
+    $class = new ReflectionObject($this);
+    try {
+      $method = $class->getMethod ('action_' . $action);
+    } catch (ReflectionException $e) {
+      throw new BaseException('Class <b>' . $class->getName () . "</b> can't handle action <b>$action</b>.",
+        Status::ERROR);
+    }
+    $this->responseData = $method->invoke ($this, $data, $param);
+  }
 }
