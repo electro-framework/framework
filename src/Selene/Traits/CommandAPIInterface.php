185c4730bbb0fb18f937208ce290a401b9d691d1
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-20 05:54:24 +0100

    Refactored task runner to support programmatic invocation and support being runned as as composer script. Minor API changes.

--- src/Selene/Traits/CommandAPIInterface.php
+++ src/Selene/Traits/CommandAPIInterface.php
@@ -16,6 +16,22 @@ trait CommandAPIInterface
    */
   abstract protected function app ();
 
+  abstract protected function ask ($question, $hideAnswer = false);
+
+  abstract protected function askDefault ($question, $default);
+
+  abstract protected function askHidden ($question);
+
+  abstract protected function banner ($text, $length = 0);
+
+  abstract protected function clear ();
+
+  abstract protected function comment ($text);
+
+  abstract protected function confirm ($question);
+
+  abstract protected function done ($text);
+
   /**
    * Prints an error message and stops execution. Use only on commands, not on tasks.
    * @param string $text  The message.
@@ -23,47 +39,33 @@ trait CommandAPIInterface
    */
   abstract protected function error ($text, $width = 0);
 
-  abstract protected function write ($text);
-
-  abstract protected function writeln ($text = '');
-
-  abstract protected function title ($text);
-
-  abstract protected function comment ($text);
-
-  abstract protected function done ($text);
-
-  abstract protected function nl ();
-
-  abstract protected function clear ();
-
   /**
-  * @return OutputInterface
-  */
-  abstract protected function getOutput();
+   * @return FilesystemStack
+   */
+  abstract protected function fs ();
+
+  abstract protected function getDialog ();
 
   /**
    * @return InputInterface
    */
-  abstract protected function getInput();
-
-  abstract protected function say($text);
-
-  abstract protected function yell($text, $length = 0);
-
-  abstract protected function ask($question, $hideAnswer = false);
-
-  abstract protected function askHidden($question);
-
-  abstract protected function askDefault($question, $default);
-
-  abstract protected function confirm($question);
-
-  abstract protected function getDialog();
+  abstract protected function getInput ();
 
   /**
-   * @return FilesystemStack
+   * @return OutputInterface
    */
-  abstract protected function fs();
+  abstract protected function getOutput ();
+
+  abstract protected function nl ();
+
+  abstract protected function say ($text);
+
+  abstract protected function title ($text);
+
+  abstract protected function write ($text);
+
+  abstract protected function writeln ($text = '');
+
+  abstract protected function yell ($text, $length = 0);
 
 }
