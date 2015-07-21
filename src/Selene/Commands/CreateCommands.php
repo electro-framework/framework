fb511cb9964a93bc33b141501a09a86babf79271
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-21 12:17:10 +0100

    Save changes to application.ini when (un)registering modules. Automactic composer dumpautoload when creating/removing modules. FIX: generated modules bug.

--- src/Selene/Commands/CreateCommands.php
+++ src/Selene/Commands/CreateCommands.php
@@ -13,18 +13,21 @@ trait CreateCommands
 
   /**
    * Scaffolds a new module for your application
+   * @param string $moduleName The full name (vendor-name/module-name) of the module to be created
    */
-  function createModule ()
+  function createModule ($moduleName = null)
   {
-    $___MODULE___ = $this->askDefault ("Module name", "vendor-name/module-name");
+    $___MODULE___ = $moduleName ?: $this->askDefault ("Module name", "vendor-name/module-name");
     $o        = explode ('/', $___MODULE___);
     if (count ($o) != 2)
       $this->error ("Invalid module name");
     list ($vendor, $module) = $o;
     $___NAMESPACE___ = ucfirst (dehyphenate ($vendor));
     $___CLASS___     = ucfirst (dehyphenate ($module));
-    $___NAMESPACE___ = $this->askDefault ("PHP namespace for the module's classes", $___NAMESPACE___);
-    $___CLASS___     = $this->askDefault ("Name of the class that represents the module:", $___CLASS___);
+    if (!$moduleName) {
+      $___NAMESPACE___ = $this->askDefault ("PHP namespace for the module's classes", $___NAMESPACE___);
+      $___CLASS___     = $this->askDefault ("Name of the class that represents the module:", $___CLASS___);
+    }
 
     $path = "{$this->app()->modulesPath}/$___MODULE___";
     if (file_exists ($path) || file_exists ("{$this->app()->defaultModulesPath}/$___MODULE___"))
@@ -48,6 +51,10 @@ trait CreateCommands
     (new Replace ("$path/bootstrap.php"))->from ($from)->to ($to)->run ();
 
     $this->done ("Module <info>$___MODULE___</info> created");
+
+    /** @var ModuleCommands $self */
+    $self = $this;
+    $self->moduleRegister($___MODULE___);
   }
 
 }
--- src/Selene/Commands/ModuleCommands.php
+++ src/Selene/Commands/ModuleCommands.php
@@ -1,5 +1,6 @@
 <?php
 namespace Selene\Commands;
+use Robo\Task\Composer\DumpAutoload;
 use Selene\Traits\CommandAPIInterface;
 
 /**
@@ -11,7 +12,7 @@ trait ModuleCommands
 
   /**
    * Registers a module on the application's configuration, therefore enabling it for use
-   * @param string $moduleName The full name (vendor-name/module-name) of the module to be enabled..
+   * @param string $moduleName The full name (vendor-name/module-name) of the module to be registered
    */
   function moduleRegister ($moduleName = '')
   {
@@ -25,14 +26,16 @@ trait ModuleCommands
       return $modules;
     });
 
+    (new DumpAutoload())->run ();
+
     $this->done ("Module <info>$moduleName</info> was registered");
   }
 
   /**
    * Removes a module from the application's configuration, therefore disabling it
-   * @param string $moduleName The full name (vendor-name/module-name) of the module to be disabled.
+   * @param string $moduleName The full name (vendor-name/module-name) of the module to be unregistered
    */
-  function moduleUnregister ($moduleName = '')
+  function moduleUnregister ($moduleName = null)
   {
     if ($moduleName && !strpos ($moduleName, '/'))
       $this->error ("Invalid module name");
@@ -54,19 +57,22 @@ trait ModuleCommands
       }
     );
 
+    (new DumpAutoload())->run ();
+
     $this->done ("Module <info>$moduleName</info> was unregistered");
   }
 
   private function changeModules (callable $fn)
   {
-    $ini = file_get_contents ($this->app ()->configPath . "/application.ini.php");
-    $ini = $this->config_modifyArray ($ini, 'modules', $c, $fn);
+    $path = $this->app ()->configPath . "/application.ini.php";
+    $ini = file_get_contents ($path);
+    $ini = $this->config_modifyArrayProperty ($ini, 'modules', $c, $fn);
     if (!$c)
       $this->error ("Can't parse the configuration file. Please reformat it and make sure there is a 'modules' key");
-    echo $ini;
+    file_put_contents ($path, $ini);
   }
 
-  private function config_modifyArray ($src, $key, &$count, callable $fn)
+  private function config_modifyArrayProperty ($src, $key, &$count, callable $fn)
   {
     return preg_replace_callback ('/^(\s*)([\'"]' . $key . '[\'"]\s*=>\s*)(\[[^]]*])/m', function ($m) use ($fn) {
       list (, $indent, $pre, $value) = $m;
