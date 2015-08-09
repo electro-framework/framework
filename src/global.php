b857fb9f5c56ae9856abc1e67f54e6bbf6755881
Cláudio Silva <claudio.silva@impactwave.com>
2015-08-09 22:47:03 +0100

    Remove global utils.

--- src/global.php
+++ src/global.php
@@ -57,78 +57,6 @@ function parseMethodRef ($ref)
   return array_merge (is_array ($ref) ? $ref : explode ('::', $ref), [null]);
 }
 
-function check_syntax ($code, &$output = 0)
-{
-  $b = 0;
-  foreach (token_get_all ($code) as $token)
-    if ('{' == $token)
-      ++$b;
-    elseif ('}' == $token)
-      --$b;
-  if ($b)
-    return false; // Unbalanced braces would break the eval below
-  ob_start (); // Catch potential parse error messages
-  $code = eval('if(0){' . $code . '}'); // Put $code in a dead code sandbox to prevent its execution
-  if ($output != 0)
-    $output = ob_get_clean ();
-  else
-    ob_end_clean ();
-  return false !== $code;
-}
-
-function firstNonNull ($a = null, $b = null, $c = null, $d = null)
-{
-  if (isset($a)) return $a;
-  if (isset($b)) return $b;
-  if (isset($c)) return $c;
-  if (isset($d)) return $d;
-  return null;
-}
-
-function ifset ($exp, $a, $b = null)
-{
-  if (isset($exp) && $exp !== '')
-    return $a;
-  return $b;
-}
-
-function iftrue ($exp, $a, $b = null)
-{
-  return $exp ? $a : $b;
-}
-
-function when ($exp, $a, $b = null)
-{
-  return $exp ? $a : $b;
-}
-
-function enum ($delimiter)
-{
-  $r = [];
-  $t = func_num_args ();
-  for ($n = 1; $n < $t; ++$n) {
-    $v = func_get_arg ($n);
-    if (!empty($v))
-      $r[] = $v;
-  }
-  return join ($delimiter, $r);
-}
-
-/**
- * Concatenates array
- * @param array $a
- * @param array $b
- */
-function concat (array &$a, array $b)
-{
-  $a = array_merge ($a, $b);
-}
-
-function convertToInt (&$variable)
-{
-  $variable = intval ($variable);
-}
-
 function param ($name)
 {
   return isset($_REQUEST[$name]) && $_REQUEST[$name] != '' ? $_REQUEST[$name] : null;
@@ -152,102 +80,9 @@ function safeParameter ($name)
   return $v;
 }
 
-function dirnameEx ($path)
-{
-  $path = dirname($path);
-  return $path == '/' ? '' : $path;
-}
-
-function trimText ($text, $maxSize)
-{
-  if (strlen ($text) <= $maxSize)
-    return $text;
-  $a = explode (' ', substr ($text, 0, $maxSize));
-  array_pop ($a);
-  return join (' ', $a) . ' (...)';
-}
-
-function trimHTMLText ($text, $maxSize)
-{
-  if (strlen ($text) <= $maxSize)
-    return $text;
-  $text = substr ($text, 0, $maxSize);
-  $a    = strrpos ($text, '>');
-  $b    = strrpos ($text, '<');
-  if ($b !== false && ($a === false || $a < $b))
-    $text = substr ($text, 0, $b);
-  $a = explode (' ', $text);
-  array_pop ($a);
-  $text = join (' ', $a) . ' (...)';
-  $c    = 0;
-  $tags = [];
-  if (preg_match_all ('#<.*?>#', $text, $matches)) {
-    foreach ($matches[0] as $match)
-      if (substr ($match, 1, 1) == '/')
-        array_pop ($tags);
-      else if (substr ($match, -2, 1) != '/')
-        array_push ($tags, trim (substr ($match, 1, strlen ($match) - 2)));
-    $tags = array_reverse ($tags);
-    foreach ($tags as $tag) {
-      $a = strpos ($tag, ' ');
-      if ($a)
-        $tag = substr ($tag, 0, $a);
-      $text .= "</$tag>";
-    }
-  }
-  return $text;
-}
-
 function extractP ($html)
 {
   return preg_match ('#<p>([\s\S]*?)</p>#', $html, $matches) ? $matches[1] : '';
-}
-
-function fileExists ($filename, $useIncludePath = true)
-{
-  return $useIncludePath ? boolval (stream_resolve_include_path ($filename)) : file_exists ($filename);
-}
-
-/**
- * Loads and executes the specified PHP file, searching for it on the 'include path'.
- * @param string $filename
- * @return bool|mixed
- */
-function includeFile ($filename) {
-  $path = stream_resolve_include_path ($filename);
-  return $path ? require $path : false;
-}
-
-// Already defined by Matisse
-if (!function_exists ('loadFile')) {
-  function loadFile ($filename, $useIncludePath = true)
-  {
-    if ($useIncludePath) {
-      if (!($filename = stream_resolve_include_path ($filename))) return false;;
-    }
-    else if (!file_exists ($filename)) return false;
-    return removeBOM (file_get_contents ($filename));
-  }
-}
-
-// Already defined by Matisse
-if (!function_exists ('removeBOM')) {
-  function removeBOM ($string)
-  {
-    if (substr ($string, 0, 3) == pack ('CCC', 0xef, 0xbb, 0xbf))
-      $string = substr ($string, 3);
-    return $string;
-  }
-}
-
-function evalPHP ($code)
-{
-  $code = trim ($code, " \n\r\t");
-  if (substr ($code, -2) != '?>')
-    $code .= '?>';
-  if (substr ($code, 0, 5) == '<?php')
-    return eval(substr ($code, 5, strlen ($code) - 7));
-  return eval($code);
 }
 
 function toURISafeText ($text)
@@ -258,68 +93,11 @@ function toURISafeText ($text)
 }
 
 /**
- * Creates a temporary directory.
- *
- * @param        $dir
- * @param string $prefix
- * @param int    $mode
- *
- * @return string
- */
-function tempdir ($dir, $prefix = '', $mode = 0700)
-{
-  if (substr ($dir, -1) != '/') $dir .= '/';
-  do {
-    $path = $dir . $prefix . mt_rand (0, 9999999);
-  } while (!mkdir ($path, $mode));
-
-  return $path;
-}
-
-/**
- * Call startProfiling() (native function) before the code being measured.
- * Call this function after the code block to display the ellapsed time.
- */
-function stopProfiling ()
-{
-  ob_clean ();
-  endProfiling ();
-  exit;
-}
-
-/**
  * @return ConsolePanel
  */
 function _log ()
 {
   $args = array_merge (['<#log>'], func_get_args ());
   return call_user_func_array ([WebConsole::$class, 'log'], $args)->showCallLocation ()->log ('</#log>');
-}
-
-/**
- * Performs padding on strings having embedded color tags.
- * > Ex: `"<color-name>text</color-name>"`
- * @param string $str
- * @param int    $width The desired minimum width, in characters.
- * @param int    $align One of the STR_PAD_XXX constants.
- * @return string
- */
-function coloredStrPad ($str, $width, $align = STR_PAD_RIGHT)
-{
-  $w    = coloredStrLen ($str);
-  $rawW = mb_strlen ($str);
-  $d    = $rawW - $w;
-  return mb_str_pad ($str, $width + $d, ' ', $align);
-}
-
-/**
- * Returns the true length of strings having embedded color tags.
- * > Ex: `"<color-name>text</color-name>"`
- * @param string $str
- * @return int The string's length, in characters.
- */
-function coloredStrLen ($str)
-{
-  return mb_strlen (preg_replace ('/<[^>]*>/u', '', $str));
 }
 
