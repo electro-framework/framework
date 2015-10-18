4666efbd4d44e1e0f183ff6d86edfe9c50265e49
Cláudio Silva <claudio.silva@impactwave.com>
2015-10-18 19:16:33 +0100

    Added web-server subsystem.

---
+++ subsystems/web-server/WebServer/WebServer.php
@@ -0,0 +1,67 @@
+<?php
+namespace Selenia\WebServer;
+use Selenia\Interfaces\InjectorInterface;
+
+/**
+ * Responds to an HTTP request made to the web application by loading and running all the framework subsystems required
+ * to handle that request.
+ */
+class WebServer
+{
+  /**
+   * @var InjectorInterface
+   */
+  private $injector;
+
+  /**
+   * @param mixed $injector An <kbd>InjectorInterface</kbd>-compatible injector instance.
+   */
+  function __construct ($injector)
+  {
+    $this->injector = $injector;
+  }
+
+  /**
+   * Runs the web server's request handler.
+   * @param string $rootDir The application's root directory path.
+   */
+  function run (string $rootDir)
+  {
+
+    $routeLoader = new RouteLoader();
+    $routes      = $routeLoader->loadFromXml (CONTROLLER_ROUTES);
+    $router      = new Router($routes);
+
+    $requestDetector = new RequestDetector();
+    $request         = $requestDetector->detectFromSuperglobal ($_SERVER);
+
+    $requestUri    = $request->getUri ();
+    $requestMethod = strtolower ($request->getMethod ());
+
+    $this->injector->share ($request);
+
+    try {
+      if (!$controllerClass = $router->route ($requestUri, $requestMethod)) {
+        throw new NoRouteMatchException();
+      }
+
+      $controller         = $this->injector->make ($controllerClass);
+      $callableController = [$controller, $requestMethod];
+
+      if (!is_callable ($callableController)) {
+        throw new MethodNotAllowedException();
+      }
+      else {
+        $callableController();
+      }
+
+    } catch (NoRouteMatchException $e) {
+      // send 404 response
+    } catch (MethodNotAllowedException $e) {
+      // send 405 response
+    } catch (Exception $e) {
+      // send 500 response
+    }
+  }
+
+}
