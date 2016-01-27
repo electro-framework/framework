c79e621b926dfa90073e95a3e4ca8b48a2b09307
Cláudio Silva <claudio.silva@impactwave.com>
2016-01-27 10:50:52 +0000

    Extracted the "common" subsystem into a separate package. Removed currently irrelevant stuff from composer.json.

--- subsystems/common/Exceptions/Fatal/ConfigException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Selenia\Exceptions\Fatal;
-
-use Selenia\Exceptions\FatalException;
-
-class ConfigException extends FatalException {
-}
--- subsystems/common/Traits/ConfigurationTrait.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Selenia\Traits;
-
-trait ConfigurationTrait
-{
-  use FluentTrait, AssignableTrait, InspectionTrait;
-
-}
