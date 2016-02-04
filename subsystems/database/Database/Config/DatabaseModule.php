4784a0e1acdaac4fdcf7bbab190ad18bf8cfd3dc
Cláudio Silva <claudio.silva@impactwave.com>
2016-02-04 18:00:04 +0000

    Use PHP-Kit/ExtPDO

--- subsystems/database/Database/Config/DatabaseModule.php
+++ subsystems/database/Database/Config/DatabaseModule.php
@@ -1,6 +1,7 @@
 <?php
 namespace Selenia\Database\Config;
 
+use PhpKit\Connection;
 use Selenia\Interfaces\InjectorInterface;
 use Selenia\Interfaces\ServiceProviderInterface;
 
@@ -8,7 +9,7 @@ class DatabaseModule implements ServiceProviderInterface
 {
   function register (InjectorInterface $injector)
   {
-
+    $injector->share ((new Connection)->getFromEnviroment ());
   }
 
 }
