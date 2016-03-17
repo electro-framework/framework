47121b8b1b584d7e44dd02d4c31cb2e1ad1c3acd
Cláudio Silva <claudio.silva@impactwave.com>
2016-03-17 13:16:12 +0000

    page() shortcut.

--- global.php
+++ global.php
@@ -1,7 +1,29 @@
 <?php
+use Selenia\Http\Components\PageComponent;
 use Selenia\Routing\Lib\FactoryRoutable;
 
+/**
+ * Generates a routable that, when invoked, will return a generic PageComponent with the specified template as a view.
+ *
+ * <p>Use this to define routes for simple pages that have no controller logic.
+ *
+ * @param string $templateUrl
+ * @return FactoryRoutable
+ */
+function page ($templateUrl)
+{
+  return new FactoryRoutable (function (PageComponent $page) use ($templateUrl) {
+    $page->templateUrl = $templateUrl;
+    return $page;
+  });
+}
 
+/**
+ * A shortcut to create a {@see FactoryRoutable}.
+ *
+ * @param callable $fn
+ * @return FactoryRoutable
+ */
 function factory (callable $fn)
 {
   return new FactoryRoutable ($fn);
@@ -16,14 +38,6 @@ function parseMethodRef ($ref)
   if (empty($ref))
     return [null, null];
   return array_merge (is_array ($ref) ? $ref : explode ('::', $ref), [null]);
-}
-
-function safeParameter ($name)
-{
-  $v = get ($_REQUEST, $name);
-  if (!empty($v))
-    $v = addslashes ($v);
-  return $v;
 }
 
 /**
--- subsystems/routing/Routing/Lib/FactoryRoutable.php
+++ subsystems/routing/Routing/Lib/FactoryRoutable.php
@@ -9,6 +9,8 @@ use Selenia\Interfaces\InjectorInterface;
  * When the router is about to invoke a routable, it checks if it is an instance of this class and, if it is, instead
  * of calling it with the standard request handler arguments `($request, $response, $next)`, it invokes it with a single
  * injector argument and then proceeds to handle the result of the call.
+ *
+ * The factory function itself can have any injectable parameters you wish. It should return a 'routable'.
  */
 class FactoryRoutable
 {
