f2e8b7bd818dc98a83f101ffba358208dfe6f0fb
Cláudio Silva <claudio.silva@impactwave.com>
2016-06-14 23:35:03 +0100

    Moved the Matisse engine to a plugin.

--- subsystems/matisse/Matisse/Exceptions/FilterHandlerNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Selenia\Matisse\Exceptions;
-
-/**
- * Indicates that a handler function/method for the required filter function is not defined.
- */
-class FilterHandlerNotFoundException extends \Exception
-{
-}
