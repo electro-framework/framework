57448af0be0c14a0d3f7947681ca7796ca4650cb
Cláudio Silva <claudio.silva@impactwave.com>
2016-06-27 18:47:55 +0100

    FIX: mail module.

---
+++ subsystems/mail/Mail/Config/EmailModule.php
@@ -0,0 +1,27 @@
+<?php
+namespace Electro\Mail\Config;
+
+use Electro\Interfaces\DI\InjectorInterface;
+use Electro\Interfaces\DI\ServiceProviderInterface;
+use Swift_Mailer;
+
+class EmailModule implements ServiceProviderInterface
+{
+  function register (InjectorInterface $injector)
+  {
+    $injector
+      ->delegate (Swift_Mailer::class, function () use ($injector) {
+        $transport = new \Swift_SmtpTransport(
+          env ('EMAIL_SMTP_HOST', 'localhost'),
+          env ('EMAIL_SMTP_PORT', 25)
+        );
+        if (env ('EMAIL_SMTP_AUTH') == 'true')
+          $transport
+            ->setUsername (env ('EMAIL_SMTP_USERNAME'))
+            ->setPassword (env ('EMAIL_SMTP_PASSWORD'));
+
+        return new Swift_Mailer ($transport);
+      });
+  }
+
+}
