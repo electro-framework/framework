7e130a0b9c52a0555cc24be88e54ba0198c55fc5
Cláudio Silva <claudio.silva@impactwave.com>
2016-07-27 17:52:02 +0100

    FIX: Dev server index page styling and bugs.

--- devServerRouter.php
+++ devServerRouter.php
@@ -1,20 +1,19 @@
 <?php
 // Router for the PHP built in server that shows directory listings.
 
-call_user_func (function () {
+return call_user_func (function () {
 
   $path = $_SERVER['DOCUMENT_ROOT'] . $_SERVER["REQUEST_URI"];
   $uri  = $_SERVER["REQUEST_URI"];
 
   // let server handle files or 404s
-  if (!file_exists ($path) || is_file ($path)) {
+  if (!file_exists ($path) || is_file ($path))
     return false;
-  }
 
   // append / to directories
   if (is_dir ($path) && $uri[strlen ($uri) - 1] != '/') {
     header ('Location: ' . $uri . '/');
-    exit;
+    return;
   }
 
   // send index.html and index.php
@@ -23,7 +22,7 @@ call_user_func (function () {
     $file = $path . '/' . $index;
     if (is_file ($file)) {
       require $file;
-      exit;
+      return;
     }
   }
 
@@ -34,11 +33,10 @@ call_user_func (function () {
     <polygon points="448 288 256 64 64 288 112 288 112 448 208 448 208 320 304 320 304 448 400 448 400 288 "/>
   </symbol>
   <symbol id="folder" viewBox="0 0 512 512">
-    <path d="M430.1 192H81.9c-17.7 0-18.6 9.2-17.6 20.5l13 183c0.9 11.2 3.5 20.5 21.1 20.5h316.2c18 0 20.1-9.2 21.1-20.5l12.1-185.3C448.7 199 447.8 192 430.1 192z"/>
-    <path d="M426.2 143.3c-0.5-12.4-4.5-15.3-15.1-15.3 0 0-121.4 0-143.2 0 -21.8 0-24.4 0.3-40.9-17.4C213.3 95.8 218.7 96 190.4 96c-22.6 0-75.3 0-75.3 0 -17.4 0-23.6-1.5-25.2 16.6 -1.5 16.7-5 57.2-5.5 63.4h343.4L426.2 143.3z"/>
+    <path fill=#DDD stroke-width=40 d="M213 96H75C51 96 32 115 32 139v235C32 397 51 416 75 416h363C461 416 480 397 480 373V187C480 163 461 144 437 144H256L213 96z"/>
   </symbol>
-  <symbol id="file" viewBox="0 0 512 512">
-    <path d="M399.3 168.9c-0.7-2.9-2-5-3.5-6.8l-83.7-91.7c-1.9-2.1-4.1-3.1-6.6-4.4 -2.9-1.5-6.1-1.6-9.4-1.6H136.2c-12.4 0-23.7 9.6-23.7 22.9v335.2c0 13.4 11.3 25.9 23.7 25.9h243.1c12.4 0 21.2-12.5 21.2-25.9V178.4C400.5 174.8 400.1 172.2 399.3 168.9zM305.5 111l58 63.5h-58V111zM144.5 416.5v-320h129v81.7c0 14.8 13.4 28.3 28.1 28.3h66.9v210H144.5z"/>
+  <symbol id="file" viewBox="0 32 512 450">
+    <path fill=#FFF stroke-width=40 d="M288 48H136c-22.1 0-40 17.9-40 40v336c0 22.1 17.9 40 40 40h240c22.1 0 40-17.9 40-40V176L288 48zM272 192V80l112 112H272z"/>
   </symbol>
   <symbol id="back" viewBox="0 0 512 512">
     <path d="M256 213.7L256 213.7 256 213.7l174.2 167.2c4.3 4.2 11.4 4.1 15.8-0.2l30.6-29.9c4.4-4.3 4.5-11.3 0.2-15.5L264.1 131.1c-2.2-2.2-5.2-3.2-8.1-3 -3-0.1-5.9 0.9-8.1 3L35.2 335.3c-4.3 4.2-4.2 11.2 0.2 15.5L66 380.7c4.4 4.3 11.5 4.4 15.8 0.2L256 213.7z"/>
@@ -109,6 +107,8 @@ nav a img {
   margin-right: 10px;
 }
 nav a svg {
+  fill: #666;
+  stroke: #666;
   vertical-align: bottom;
   margin-right: 6px;
 }
@@ -153,7 +153,7 @@ $SVGs
 ";
 
   if ($uri != '/')
-    echo "<a href='..'>{$BACK_ICON}Parent directory</a>";
+    echo "<a href='..'>{$BACK_ICON}..</a>";
 
   $g = array_map (function ($path) {
     if (is_dir ($path)) {
