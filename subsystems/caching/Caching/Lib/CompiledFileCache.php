37cfbfbf05ce5cd3de61493ca511330f366bb7da
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-18 00:31:32 +0000

    FileSystemCache.

---
+++ subsystems/caching/Caching/Lib/CompiledFileCache.php
@@ -0,0 +1,88 @@
+<?php
+namespace Electro\Caching\Lib;
+
+use Electro\Interfaces\Caching\CacheInterface;
+
+class CompiledFileCache implements CacheInterface
+{
+  /**
+   * @var CacheInterface The underlying cache instance.
+   */
+  protected $cache;
+
+  public function __construct (CacheInterface $underlyingCache) {
+    $this->cache = $underlyingCache;
+  }
+
+
+  function add ($key, $value)
+  {
+
+  }
+
+
+  function clear ()
+  {
+
+  }
+
+
+  function delete ($key)
+  {
+
+  }
+
+
+  function get ($key, $value)
+  {
+
+  }
+
+
+  function getNamespace ()
+  {
+
+  }
+
+
+  function has ($key)
+  {
+
+  }
+
+
+  function inc ($key, $value = 1)
+  {
+
+  }
+
+
+  function prune ()
+  {
+
+  }
+
+
+  function set ($key, $value)
+  {
+
+  }
+
+
+  function setNamespace ($name)
+  {
+
+  }
+
+
+  function setOptions (array $options)
+  {
+
+  }
+
+
+  function with (array $options)
+  {
+
+  }
+}
