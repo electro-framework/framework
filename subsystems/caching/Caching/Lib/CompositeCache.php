cc4c2e7da33718d5a7486dcc86d066b04661d377
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-23 01:13:33 +0000

    CompositeCache.

---
+++ subsystems/caching/Caching/Lib/CompositeCache.php
@@ -0,0 +1,75 @@
+<?php
+namespace Electro\Caching\Lib;
+
+use Electro\Interfaces\Caching\CacheInterface;
+
+class CompositeCache implements CacheInterface
+{
+  public function __construct () { }
+
+
+  function add ($key, $value)
+  {
+
+  }
+
+  function clear ()
+  {
+
+  }
+
+  function get ($key, $value = null)
+  {
+
+  }
+
+  function getNamespace ()
+  {
+
+  }
+
+  function getTimestamp ($key)
+  {
+
+  }
+
+  function has ($key)
+  {
+
+  }
+
+  function inc ($key, $value = 1)
+  {
+
+  }
+
+  function prune ()
+  {
+
+  }
+
+  function remove ($key)
+  {
+
+  }
+
+  function set ($key, $value)
+  {
+
+  }
+
+  function setNamespace ($name)
+  {
+
+  }
+
+  function setOptions (array $options)
+  {
+
+  }
+
+  function with (array $options)
+  {
+
+  }
+}
