5939149fb2ede37340cfe53564afcccd3d1cc03c
Cláudio Silva <claudio.silva@impactwave.com>
2017-08-28 11:42:17 +0100

    Reindenting.

--- subsystems/localization/Localization/Middleware/LanguageMiddleware.php
+++ subsystems/localization/Localization/Middleware/LanguageMiddleware.php
@@ -15,66 +15,71 @@ use Psr\Http\Message\ServerRequestInterface;
 /**
  *
  */
-class LanguageMiddleware implements RequestHandlerInterface {
+class LanguageMiddleware implements RequestHandlerInterface
+{
 
-    /**
-     * @var DebugSettings
-     */
-    private $debugSettings;
+  /**
+   * @var DebugSettings
+   */
+  private $debugSettings;
 
-    /**
-     * @var Locale
-     */
-    private $locale;
+  /**
+   * @var Locale
+   */
+  private $locale;
 
-    /**
-     * @var SessionInterface
-     */
-    private $session;
+  /**
+   * @var SessionInterface
+   */
+  private $session;
 
-    /**
-     * @var LocalizationSettings
-     */
-    private $settings;
+  /**
+   * @var LocalizationSettings
+   */
+  private $settings;
 
-    /**
-     * LanguageMiddleware constructor.
-     *
-     * @param SessionInterface     $session
-     * @param Locale               $locale
-     * @param LocalizationSettings $settings
-     * @param bool $webConsole
-     */
-    function __construct(SessionInterface $session, Locale $locale, LocalizationSettings $settings, DebugSettings $debugSettings) {
-        $this->session = $session;
-        $this->locale = $locale;
-        $this->settings = $settings;
-        $this->debugSettings = $debugSettings;
+  /**
+   * LanguageMiddleware constructor.
+   *
+   * @param SessionInterface     $session
+   * @param Locale               $locale
+   * @param LocalizationSettings $settings
+   * @param bool                 $webConsole
+   */
+  function __construct (SessionInterface $session, Locale $locale, LocalizationSettings $settings,
+                        DebugSettings $debugSettings)
+  {
+    $this->session       = $session;
+    $this->locale        = $locale;
+    $this->settings      = $settings;
+    $this->debugSettings = $debugSettings;
+  }
+
+  function __invoke (ServerRequestInterface $request, ResponseInterface $response, callable $next)
+  {
+    $mode = $this->settings->selectionMode ();
+    $this->locale->selectionMode ($mode);
+
+    if ($mode == 'session')
+      $lang = $this->session->getLang () ?: $this->locale->defaultLang ();
+    else {
+      $url = explode ('/', $request->getAttribute ('virtualUri'), 2);
+      if ($url[0]) {
+        $request = $request->withAttribute ('virtualUri', get ($url, 1, ''));
+        $lang    = $url[0];
+      }
+      else
+        $lang = $this->locale->defaultLang ();
     }
 
-    function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next) {
-        $mode = $this->settings->selectionMode();
-        $this->locale->selectionMode($mode);
+    if (!$this->locale->isAvailable ($lang))
+      return Http::response ($response, "Not found", 'text/html', 404);
 
-        if ($mode == 'session')
-            $lang = $this->session->getLang() ?: $this->locale->defaultLang();
-        else {
-            $url = explode('/', $request->getAttribute('virtualUri'), 2);
-            if ($url[0]) {
-                $request = $request->withAttribute('virtualUri', get($url, 1, ''));
-                $lang = $url[0];
-            } else
-                $lang = $this->locale->defaultLang();
-        }
+    $this->locale->locale ($lang);
+    $this->session->setLang ($lang);
 
-        if (!$this->locale->isAvailable($lang))
-            return Http::response($response, "Not found", 'text/html', 404);
-
-        $this->locale->locale($lang);
-        $this->session->setLang($lang);
-
-        if ($this->debugSettings->logConfig)
-            DebugConsole::logger('config')->inspect($this->locale);
-        return $next($request);
-    }
+    if ($this->debugSettings->logConfig)
+      DebugConsole::logger ('config')->inspect ($this->locale);
+    return $next($request);
+  }
 }
