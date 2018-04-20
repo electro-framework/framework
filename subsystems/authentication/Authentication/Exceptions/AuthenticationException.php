168ca7d19321c4d0ecb09a6728b0981f4a772131
Cláudio Silva <claudio.silva@impactwave.com>
2018-04-20 12:16:55 +0100

    Remove obsolete constants again.

--- subsystems/authentication/Authentication/Exceptions/AuthenticationException.php
+++ subsystems/authentication/Authentication/Exceptions/AuthenticationException.php
@@ -6,18 +6,6 @@ use Electro\Exceptions\FlashType;
 
 class AuthenticationException extends FlashMessageException
 {
-  const DISABLED       = 4;
-  const MISSING_INFO   = 1;
-  const UNKNOWN_USER   = 2;
-  const WRONG_PASSWORD = 3;
-
-  public static $messages = [
-    self::MISSING_INFO   => '$LOGIN_MISSING_INFO',
-    self::UNKNOWN_USER   => '$LOGIN_UNKNOWN_USER',
-    self::WRONG_PASSWORD => '$LOGIN_WRONG_PASSWORD',
-    self::DISABLED       => '$LOGIN_DISABLED',
-  ];
-
   public function __construct ($message, $status = FlashType::WARNING, $title = '')
   {
     parent::__construct ($message, $status, $title);
