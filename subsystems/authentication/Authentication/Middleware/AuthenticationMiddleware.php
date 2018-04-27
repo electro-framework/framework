6ab050e8c1ff97400f6860540278732591cd3bcc
Tiago Elias <tiagoelias1999@gmail.com>
2018-04-27 10:16:24 +0100

    Handle expired session based on rememberMe token

--- subsystems/authentication/Authentication/Middleware/AuthenticationMiddleware.php
+++ subsystems/authentication/Authentication/Middleware/AuthenticationMiddleware.php
@@ -6,7 +6,11 @@ use Electro\Authentication\Exceptions\AuthenticationException;
 use Electro\Interfaces\Http\RedirectionInterface;
 use Electro\Interfaces\Http\RequestHandlerInterface;
 use Electro\Interfaces\SessionInterface;
+use Electro\Interfaces\UserInterface;
 use Electro\Kernel\Config\KernelSettings;
+use GuzzleHttp\Psr7\ServerRequest;
+use HansOtt\PSR7Cookies\RequestCookies;
+use HansOtt\PSR7Cookies\SetCookie;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -27,34 +31,56 @@ class AuthenticationMiddleware implements RequestHandlerInterface
    * @var AuthenticationSettings
    */
   private $settings;
+  /**
+   * @var UserInterface
+   */
+  private $user;
 
   function __construct (KernelSettings $kernelSettings, SessionInterface $session, RedirectionInterface $redirection,
-                        AuthenticationSettings $settings)
+                        AuthenticationSettings $settings, UserInterface $user)
   {
     $this->session        = $session;
     $this->kernelSettings = $kernelSettings;
     $this->redirection    = $redirection;
     $this->settings       = $settings;
+    $this->user           = $user;
   }
 
   function __invoke (ServerRequestInterface $request, ResponseInterface $response, callable $next)
   {
     $this->redirection->setRequest ($request);
+    $serverRequest = ServerRequest::fromGlobals ();
+    $cookies       = RequestCookies::createFromRequest ($serverRequest);
 
     // LOG OUT
 
     if ($request->getUri () == $this->settings->getLogoutUrl ()) {
+      $response = $this->redirection->home ();
+
       $this->session->logout ();
-      return $this->redirection->home ();
+      if ($cookies->has ($this->kernelSettings->name . "/" . $this->kernelSettings->rememberMeTokenName)) {
+        $cookie   =
+          SetCookie::thatDeletesCookie ($this->kernelSettings->name . "/" . $this->kernelSettings->rememberMeTokenName,
+            $request->getAttribute ('baseUri'));
+        $response = $cookie->addToResponse ($response);
+      }
+      return $response;
     }
 
     switch ($request->getMethod ()) {
       case 'GET':
-
         // LOG IN
-
-        if (!$this->session->loggedIn ())
+        if (!$this->session->loggedIn ()) {
+          if ($cookies->has ($this->kernelSettings->name . "/" . $this->kernelSettings->rememberMeTokenName)) {
+            $token = $cookies->get ($this->kernelSettings->name . "/" . $this->kernelSettings->rememberMeTokenName)
+                             ->getValue ();
+            $user  = $this->user;
+            $user->findByToken ($token);
+            $this->session->setUser ($user);
+            return $next();
+          }
           return $this->redirection->guest ($this->settings->getLoginUrl ());
+        }
         break;
     }
 
