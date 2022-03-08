9364edd81d69679124aa2c9b06cede1a4f12f809
Cláudio Silva <claudio.silva@impactwave.com>
2022-03-08 09:32:00 +0000

    FIX: CompressionMiddleware.

--- subsystems/http/Http/Middleware/CompressionMiddleware.php
+++ subsystems/http/Http/Middleware/CompressionMiddleware.php
@@ -8,9 +8,20 @@ use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Applies gzip compression to the HTTP response.
+ *
+ * The response will be compressed only if its content type is a text-based format and the client declares support for
+ * GZIP compression.
  */
 class CompressionMiddleware implements RequestHandlerInterface
 {
+  /**
+   * Note: all content types beginning with 'text/' or ending in 'xml' (ex: 'application/xml' or 'application/xhtml+xml')
+   * are already supported, so they are not included here.
+   */
+  const ADDITIONAL_CONTENT_TYPES = [
+    'application/json',
+  ];
+
   function __construct (ResponseFactoryInterface $responseFactory)
   {
     $this->responseFactory = $responseFactory;
@@ -22,10 +33,19 @@ class CompressionMiddleware implements RequestHandlerInterface
     $response = $next();
 
     if (strpos ($request->getHeaderLine ('accept-encoding'), 'gzip') !== false) {
-      $out = gzencode ($response->getBody (), 1);
-      return $response
-        ->withHeader ('Content-Encoding', 'gzip')
-        ->withBody ($this->responseFactory->makeBodyStream ($out));
+      $contentType = $response->getHeaderLine ('content-type') ?? '';
+      if (substr($contentType, 0, 5) == 'text/' || substr($contentType, -3) == 'xml' ||
+          in_array ($contentType, self::ADDITIONAL_CONTENT_TYPES)) {
+        $uncompressed = $response->getBody ();
+        $out = gzencode ($uncompressed, 1);
+        //If the compression failed, return the uncompressed response.
+        if ($out === false)
+          $out = $uncompressed;
+        return $response
+          ->withHeader ('Content-Encoding', 'gzip')
+          ->withHeader ('Content-Length', strlen ($out))
+          ->withBody ($this->responseFactory->makeBodyStream ($out));
+      }
     }
     return $response;
   }
