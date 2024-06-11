e891f1f51e2fedf664c98b7875788b8e441eaf2c
Goncalo Martins <goncalo.martins@impactwave.com>
2024-06-11 17:02:29 +0100

    swiftmailer deprecated

--- subsystems/mail/Mail/Config/MailModule.php
+++ subsystems/mail/Mail/Config/MailModule.php
@@ -8,54 +8,33 @@ use Electro\Kernel\Lib\ModuleInfo;
 use Electro\Profiles\ApiProfile;
 use Electro\Profiles\ConsoleProfile;
 use Electro\Profiles\WebProfile;
-use Swift_Mailer;
-use Swift_Plugins_LoggerPlugin;
-use Swift_Plugins_Loggers_ArrayLogger;
 
 class MailModule implements ModuleInterface
 {
-  const MAX_LOG_SIZE = 50;
 
-  static function getCompatibleProfiles ()
-  {
-    return [WebProfile::class, ConsoleProfile::class, ApiProfile::class];
-  }
+	const MAX_LOG_SIZE = 50;
 
-  static function startUp (KernelInterface $kernel, ModuleInfo $moduleInfo)
-  {
-    $kernel->onRegisterServices (
-      function (InjectorInterface $injector) {
-        $injector
-          ->delegate (Swift_Mailer::class, function () use ($injector) {
+	static function getCompatibleProfiles()
+	{
+		return [WebProfile::class, ConsoleProfile::class, ApiProfile::class];
+	}
 
-			 $transport = $injector->make(\Swift_Transport_EsmtpTransport::class);
+	static function startUp(KernelInterface $kernel, ModuleInfo $moduleInfo)
+	{
+		$kernel->onRegisterServices(
+			function (InjectorInterface $injector)
+			{
+				$injector
+					->delegate(\Symfony\Component\Mailer\Mailer::class, function () use ($injector)
+					{
 
-			 $this->setHost(env('EMAIL_SMTP_HOST', 'localhost'));
-						$this->setPort(env('EMAIL_SMTP_PORT', 25));
-						if (env('EMAIL_SMTP_SECURE'))
-							$this->setEncryption(env('EMAIL_SMTP_SECURE'));
 
-				/* 		$transport = new \Swift_SmtpTransport(
-						  env ('EMAIL_SMTP_HOST', 'localhost'),
-						  env ('EMAIL_SMTP_PORT', 25)
-						  ); */
+						$factory = new \Symfony\Component\Mailer\Transport\Smtp\EsmtpTransportFactory();
+						$transport = $factory->create(new \Symfony\Component\Mailer\Transport\Dsn(env('EMAIL_SMTP_SECURE') ? 'smtps' : 'smtps', env('EMAIL_SMTP_HOST', 'localhost'), env('EMAIL_SMTP_USERNAME'), env('EMAIL_SMTP_PASSWORD'), env('EMAIL_SMTP_PORT', 25)));
 
-						if (env ('EMAIL_SMTP_AUTH'))
-              $transport
-                ->setUsername (env ('EMAIL_SMTP_USERNAME'))
-                ->setPassword (env ('EMAIL_SMTP_PASSWORD'));
-
-//						if (env ('EMAIL_SMTP_SECURE'))
-//								$transport->setEncryption(env ('EMAIL_SMTP_SECURE'));
-
-						$mailer = new Swift_Mailer ($transport);
-            $logger = new Swift_Plugins_Loggers_ArrayLogger (self::MAX_LOG_SIZE);
-            $mailer->registerPlugin (new Swift_Plugins_LoggerPlugin ($logger));
-            // Create run-time custom property to allow easy access to the logger.
-            $mailer->logger = $logger;
-            return $mailer;
-          });
-      });
-  }
+						return new \Symfony\Component\Mailer\Mailer($transport);
+					});
+			});
+	}
 
 }
